a4adf2436a6459b531d12c5be25440a1d966863c
andrerobsahm <andrerobsahm@users.noreply.github.com>
2018-04-13 12:19:09 +0200

    Working on checkout, done some css, cleaned up home page

---
+++ Webshop/Webshop/Controllers/CheckoutController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using Webshop.Models;
+
+namespace Webshop.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        private readonly string connectionString;
+
+        public CheckoutController(IConfiguration configuration)
+        {
+            this.connectionString = configuration.GetConnectionString("ConnectionString");
+        }
+
+
+        public IActionResult Index()
+        {
+            using (var connection = new MySqlConnection(this.connectionString))
+            {
+                var checkout = connection.Query<CheckoutViewModel>("Select * from Cart").ToList();
+
+                return View(checkout);
+                }
+
+        }
+
+
+    }
+}
---
+++ Webshop/Webshop/Models/CheckoutViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Webshop.Models;
+
+namespace Webshop.Models
+{
+    public class CheckoutViewModel
+    {
+        public List<CartViewModel> Cart { get; set; }
+
+        public int Sum { get; set; }
+    }
+}
